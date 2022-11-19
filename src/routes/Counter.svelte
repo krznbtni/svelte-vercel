826d4d18d56ac646eca6ffdc5dad3f3bde2a6aac
Karzan Botani <karzan@botani.nu>
2022-11-19 12:27:30 +0100

    added gtm datalayer events

--- src/routes/Counter.svelte
+++ src/routes/Counter.svelte
@@ -11,10 +11,22 @@
 		// handle negative numbers
 		return ((n % m) + m) % m;
 	}
+
+	function onDecrement() {
+		count -= 1;
+		window.dataLayer.push({ event: 'decrement' });
+	}
+
+	function onIncrement() {
+		count += 1;
+		window.dataLayer.push({ event: 'increment' });
+	}
+
+	function event() {}
 </script>
 
 <div class="counter">
-	<button on:click={() => (count -= 1)} aria-label="Decrease the counter by one">
+	<button on:click={onDecrement} aria-label="Decrease the counter by one">
 		<svg aria-hidden="true" viewBox="0 0 1 1">
 			<path d="M0,0.5 L1,0.5" />
 		</svg>
@@ -27,7 +39,7 @@
 		</div>
 	</div>
 
-	<button on:click={() => (count += 1)} aria-label="Increase the counter by one">
+	<button on:click={onIncrement} aria-label="Increase the counter by one">
 		<svg aria-hidden="true" viewBox="0 0 1 1">
 			<path d="M0,0.5 L1,0.5 M0.5,0 L0.5,1" />
 		</svg>
